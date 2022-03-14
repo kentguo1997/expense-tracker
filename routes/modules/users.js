71820be58574d65c780f2d378b764048f36e6886
kentguo_1997 <guoyunkai201709@gmail.com>
2022-03-14 15:49:19 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,6 +8,17 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  res.send('Login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  res.send('Register')
+})
 
 // export router for index.js
 module.exports = router
